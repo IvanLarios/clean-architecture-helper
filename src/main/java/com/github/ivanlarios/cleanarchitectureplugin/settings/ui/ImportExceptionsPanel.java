11d92e6454422b0ceee990bc61e65a3bf66161f7
Iván Larios López <ivan.larios.lopez@gmail.com>
2023-07-21 01:55:46 +0200

    Extracted swing component into new class. Added all messages to bundle to enable future translated bundles.

---
+++ src/main/java/com/github/ivanlarios/cleanarchitectureplugin/settings/ui/ImportExceptionsPanel.java
@@ -0,0 +1,112 @@
+package com.github.ivanlarios.cleanarchitectureplugin.settings.ui;
+
+import com.github.ivanlarios.cleanarchitectureplugin.settings.CleanArchitectureBundle;
+import com.intellij.openapi.ui.DialogBuilder;
+import com.intellij.openapi.util.text.StringUtil;
+import com.intellij.openapi.wm.IdeFocusManager;
+import com.intellij.ui.*;
+import com.intellij.ui.components.JBList;
+import com.intellij.ui.components.JBScrollPane;
+import com.intellij.util.ui.JBUI;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class ImportExceptionsPanel extends JPanel {
+    private final JBList<String> importList = new JBList<>(new DefaultListModel<>());
+
+    public ImportExceptionsPanel() {
+        setLayout(new BorderLayout());
+        importList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        importList.setCellRenderer(new ImportExceptionRenderer());
+        importList.getEmptyText().setText(CleanArchitectureBundle.message("cleanarchitecture.settings.exceptions.list.empty"));
+        importList.setBorder(JBUI.Borders.empty());
+
+        ToolbarDecorator decorator = ToolbarDecorator.createDecorator(importList)
+                .setScrollPaneBorder(JBUI.Borders.empty())
+                .setPanelBorder(JBUI.Borders.customLine(JBColor.border(),1,1,0,1))
+                .setAddAction(__ -> addImportException())
+                .setEditAction(__ -> editImportException())
+                .setRemoveAction(__ -> removeImportException())
+                .disableUpDownActions();
+        add(decorator.createPanel(), BorderLayout.NORTH);
+        JScrollPane scrollPane = new JBScrollPane(importList);
+        scrollPane.setBorder(JBUI.Borders.customLine(JBColor.border(), 0, 1, 1, 1));
+        add(scrollPane, BorderLayout.CENTER);
+        //noinspection DialogTitleCapitalization IDEA-254041
+        setBorder(IdeBorderFactory.createTitledBorder(CleanArchitectureBundle.message("cleanarchitecture.settings.exceptions.list.label"), false, JBUI.insetsTop(8)).setShowLine(false));
+    }
+
+    public List<String> getExceptionList() {
+        return Arrays.stream(getImportListModel().toArray()).map(Object::toString).toList();
+    }
+
+    public void setExceptionList(List<String> exceptionList) {
+        getImportListModel().clear();
+        getImportListModel().addAll(exceptionList);
+    }
+    private DefaultListModel<String> getImportListModel() {
+        return (DefaultListModel<String>) importList.getModel();
+    }
+
+    private void select(@NotNull String importException) {
+        ScrollingUtil.selectItem(importList, importException);
+    }
+
+    private void addImportException() {
+        showImportExceptionDialog(null);
+    }
+
+    private void editImportException() {
+        String selectedValue = importList.getSelectedValue();
+        if(selectedValue == null) {
+            return;
+        }
+        showImportExceptionDialog(selectedValue);
+    }
+
+    private void removeImportException() {
+        String selectedValue = importList.getSelectedValue();
+        if(selectedValue == null) {
+            return;
+        }
+        getImportListModel().removeElement(selectedValue);
+    }
+
+    private void showImportExceptionDialog(@Nullable("null means new") String item) {
+        String title = CleanArchitectureBundle.message(item == null ?
+                "cleanarchitecture.settings.exceptions.dialog.add.label" :
+                "cleanarchitecture.settings.exceptions.dialog.edit.label"
+        );
+        ImportExceptionDialog dialog = new ImportExceptionDialog(item);
+        DialogBuilder builder = new DialogBuilder(importList);
+        builder.setPreferredFocusComponent(dialog.getImportExceptionField());
+        builder.setCenterPanel(dialog.getContentPane());
+        builder.setTitle(title);
+        builder.showModal(true);
+        if(builder.getDialogWrapper().isOK()) {
+            String exception = dialog.getImportExceptionField().getText();
+            if(StringUtil.isEmpty(exception) || getImportListModel().contains(exception)){
+                return;
+            }
+            if(item != null) {
+                getImportListModel().removeElement(item);
+            }
+            getImportListModel().addElement(exception);
+            select(exception);
+            IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(() -> IdeFocusManager.getGlobalInstance().requestFocus(importList, true));
+        }
+    }
+
+    private static class ImportExceptionRenderer extends ColoredListCellRenderer<String> {
+        @Override
+        protected void customizeCellRenderer(@NotNull JList<? extends String> list, String value, int index, boolean selected, boolean hasFocus) {
+            append(value);
+        }
+    }
+}
--- src/main/java/com/github/ivanlarios/cleanarchitectureplugin/settings/ui/SettingsComponent.java
+++ src/main/java/com/github/ivanlarios/cleanarchitectureplugin/settings/ui/SettingsComponent.java
@@ -1,50 +1,46 @@
 package com.github.ivanlarios.cleanarchitectureplugin.settings.ui;
 
+import com.github.ivanlarios.cleanarchitectureplugin.settings.CleanArchitectureBundle;
 import com.github.ivanlarios.cleanarchitectureplugin.settings.PluginSettingState;
 import com.google.common.collect.ImmutableMap;
 import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.openapi.ui.ComboBox;
-import com.intellij.openapi.ui.DialogBuilder;
-import com.intellij.openapi.util.text.StringUtil;
-import com.intellij.openapi.wm.IdeFocusManager;
-import com.intellij.ui.*;
 import com.intellij.ui.components.JBCheckBox;
 import com.intellij.ui.components.JBLabel;
-import com.intellij.ui.components.JBList;
-import com.intellij.ui.components.JBScrollPane;
 import com.intellij.util.ui.FormBuilder;
-import com.intellij.util.ui.JBUI;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
-import java.awt.*;
 import java.util.*;
 import java.util.List;
 
 public class SettingsComponent {
 
-    private final String[] restrictionLevelOptions = {"Error", "Warning", "Weak warning", "Informative"};
-    private final List<String> exceptions = new ArrayList<>();
+    private final String[] restrictionLevelOptions = {
+            CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.error"),
+            CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.warning"),
+            CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.weakwarn"),
+            CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.info")
+    };
     private final static ImmutableMap<String, ProblemHighlightType> restrictionLevelToHighlightType =
             new ImmutableMap.Builder<String, ProblemHighlightType>()
-                    .put("Error", ProblemHighlightType.ERROR)
-                    .put("Warning", ProblemHighlightType.WARNING)
-                    .put("Weak warning", ProblemHighlightType.WEAK_WARNING)
-                    .put("Informative", ProblemHighlightType.INFORMATION)
+                    .put(CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.error"), ProblemHighlightType.ERROR)
+                    .put(CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.warning"), ProblemHighlightType.WARNING)
+                    .put(CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.weakwarn"), ProblemHighlightType.WEAK_WARNING)
+                    .put(CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.info"), ProblemHighlightType.INFORMATION)
                     .build();
 
     private final JPanel mainPanel;
-    private final JBCheckBox enableLinter = new JBCheckBox("Enable linter");
+    private final JBCheckBox enableLinter = new JBCheckBox(CleanArchitectureBundle.message("cleanarchitecture.settings.linter.enable.checkbox"));
     private final ComboBox<String> restrictionLevel = new ComboBox<>(restrictionLevelOptions);
-    private final JBCheckBox disallowExternalImportsInDomain = new JBCheckBox("Only allow domain imports on domain layer");
-    private final JBCheckBox disallowExternalImportsInApplication = new JBCheckBox("Only allow domain and application imports on application layer");
-    private final ImportExceptionPanel importExceptionList = new ImportExceptionPanel();
+    private final JBCheckBox disallowExternalImportsInDomain = new JBCheckBox(CleanArchitectureBundle.message("cleanarchitecture.settings.imports.domain.allow.checkbox"));
+    private final JBCheckBox disallowExternalImportsInApplication = new JBCheckBox(CleanArchitectureBundle.message("cleanarchitecture.settings.imports.application.allow.checkbox"));
+    private final ImportExceptionsPanel importExceptionList = new ImportExceptionsPanel();
 
     public SettingsComponent(PluginSettingState state) {
         mainPanel = FormBuilder.createFormBuilder()
                 .addComponent(enableLinter)
-                .addLabeledComponent(new JBLabel("Select a restriction level for inspection "), restrictionLevel, 1, false)
+                .addLabeledComponent(new JBLabel(CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.label")), restrictionLevel, 1, false)
                 .addComponent(disallowExternalImportsInDomain, 0)
                 .addComponent(disallowExternalImportsInApplication, 0)
                 .addComponent(importExceptionList)
@@ -60,7 +56,7 @@ public class SettingsComponent {
                 restrictionLevelToHighlightType.entrySet().stream()
                         .filter(r -> r.getValue().equals(state.restrictionLevel))
                         .map(Map.Entry::getKey)
-                        .findFirst().orElse("Informative");
+                        .findFirst().orElse(CleanArchitectureBundle.message("cleanarchitecture.settings.linter.level.info"));
         setRestrictionLevel(stateRestrictionLevel);
         setDisallowExternalImportsInApplication(state.disallowExternalImportsInApplication);
         setDisallowExternalImportsInDomain(state.disallowExternalImportsInDomain);
@@ -120,120 +116,11 @@ public class SettingsComponent {
     }
 
     public List<String> getExceptions(){
-        return this.exceptions;
+        return this.importExceptionList.getExceptionList();
     }
 
     public void setExceptions(List<String> exceptions){
-        this.exceptions.clear();
-        this.exceptions.addAll(exceptions);
-        this.importExceptionList.refill(this.exceptions);
-    }
-
-
-    class ImportExceptionPanel extends JPanel {
-        private final JBList<String> importList = new JBList<>(new DefaultListModel<>());
-
-        ImportExceptionPanel() {
-            setLayout(new BorderLayout());
-            importList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-            importList.setCellRenderer(new ImportExceptionRenderer());
-            importList.getEmptyText().setText("No exceptions were added."); //TODO: Add to bundle
-            importList.setBorder(JBUI.Borders.empty());
-
-            ToolbarDecorator decorator = ToolbarDecorator.createDecorator(importList)
-                    .setScrollPaneBorder(JBUI.Borders.empty())
-                    .setPanelBorder(JBUI.Borders.customLine(JBColor.border(),1,1,0,1))
-                    .setAddAction(__ -> addImportException())
-                    .setEditAction(__ -> editImportException())
-                    .setRemoveAction(__ -> removeImportException())
-                    .disableUpDownActions();
-            add(decorator.createPanel(), BorderLayout.NORTH);
-            JScrollPane scrollPane = new JBScrollPane(importList);
-            scrollPane.setBorder(JBUI.Borders.customLine(JBColor.border(), 0, 1, 1, 1));
-            add(scrollPane, BorderLayout.CENTER);
-            //noinspection DialogTitleCapitalization IDEA-254041
-            setBorder(IdeBorderFactory.createTitledBorder("Add packages exceptions", false, JBUI.insetsTop(8)).setShowLine(false));
-        }
-
-        void clearList() {
-            ((DefaultListModel<String>)importList.getModel()).clear();
-            importList.clearSelection();
-        }
-
-        void select(@NotNull String importException) {
-            ScrollingUtil.selectItem(importList, importException);
-        }
-
-        String removeSelected() {
-            String selectedValue = getSelectedItem();
-            if (selectedValue == null) return null;
-            exceptions.remove(selectedValue);
-            importExceptionList.refill(exceptions);
-            return selectedValue;
-        }
-
-        String getSelectedItem() {
-            return importList.getSelectedValue();
-        }
-
-        private void refill(@NotNull java.util.List<? extends String> exceptions) {
-            clearList();
-            List<String> copy = new ArrayList<>(exceptions);
-            copy.sort(Comparator.comparing(String::toString));
-            DefaultListModel<String> model = (DefaultListModel<String>)importList.getModel();
-            for (String exception : copy) {
-                model.addElement(exception);
-            }
-            ScrollingUtil.ensureSelectionExists(importList);
-        }
-    }
-
-    private static class ImportExceptionRenderer extends ColoredListCellRenderer<String> {
-        @Override
-        protected void customizeCellRenderer(@NotNull JList<? extends String> list, String value, int index, boolean selected, boolean hasFocus) {
-            append(value);
-        }
-    }
-
-    private void addImportException() {
-        editImportException(null);
-    }
-
-    private void editImportException() {
-        String item = importExceptionList.getSelectedItem();
-        if (item != null) {
-            editImportException(item);
-        }
-    }
-
-    private void editImportException(@Nullable("null means new") String item) {
-        String title = item == null ? "Add Exception" : "Edit Exception";
-        ImportExceptionDialog dialog = new ImportExceptionDialog(item);
-        DialogBuilder builder = new DialogBuilder(importExceptionList.importList);
-        builder.setPreferredFocusComponent(dialog.getImportExceptionField());
-        builder.setCenterPanel(dialog.getContentPane());
-        builder.setTitle(title);
-        builder.showModal(true);
-        if (builder.getDialogWrapper().isOK()) {
-            String exception = dialog.getImportExceptionField().getText();
-            if (StringUtil.isEmpty(exception)) return;
-            if(item!=null)
-                exceptions.remove(item);
-            if(!exceptions.contains(exception)) {
-                exceptions.add(exception);
-            }
-            importExceptionList.refill(exceptions);
-            importExceptionList.select(exception);
-            IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(() -> IdeFocusManager.getGlobalInstance().requestFocus(importExceptionList.importList, true));
-        }
-    }
-
-    private void removeImportException() {
-        String removed = importExceptionList.removeSelected();
-        if (removed == null) return;
-        exceptions.remove(removed);
-        importExceptionList.refill(exceptions);
-        IdeFocusManager.getGlobalInstance().doWhenFocusSettlesDown(() -> IdeFocusManager.getGlobalInstance().requestFocus(importExceptionList.importList, true));
+        this.importExceptionList.setExceptionList(exceptions);
     }
 
 }
